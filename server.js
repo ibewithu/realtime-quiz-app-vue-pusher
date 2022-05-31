d472c5742839086bab3780b3006a144f5aa27af8
bumblebee <notcoder7@gmail.com>
2022-05-31 12:01:38 +0530

    server-patch

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-
 const pusher = new Pusher({
   appId: process.env.VUE_APP_APP_ID,
   key: process.env.VUE_APP_KEY,
@@ -19,18 +18,14 @@ const pusher = new Pusher({
   encrypted: true
 });
 
+app.set('PORT', process.env.PORT || 8000);
 
-app.set('PORT', process.env.PORT || 5000);
-
-
-app.post("/pusher/auth/:username", function (req, res) {
+app.post("/pusher/auth", function (req, res) {
   const socketId = req.body.socket_id
   const channel_name = req.body.channel_name
   const authResponse = pusher.authenticate(socketId, channel_name);
   console.log('server auth', authResponse)
   res.send(authResponse);
 })
-
-
 
 app.listen(app.get('PORT'), () => console.log('Listening at ' + app.get('PORT')))
